3649f04db03fc7e308ec72195ea34ab2e04ae62f
Joe Randazzo <jprandazzo@icloud.com>
2023-05-22 16:01:20 -0400

    build_move_tree

--- Knights Travails/knights_travails.rb
+++ Knights Travails/knights_travails.rb
@@ -1,6 +1,9 @@
+require "byebug"
 require '../Poly Tree Node/lib/tree_node.rb'
 
 class KnightPathFinder
+    attr_accessor :considered_positions
+
     def self.valid_moves(pos)
         row,column = pos
         moves = [[row + 2, column + 1], 
@@ -11,32 +14,43 @@ class KnightPathFinder
                 [row - 1, column - 2],
                 [row - 2, column - 1],
                 [row - 2, column + 1]]
-        moves.select {|pos| pos[0].between?(0,8) && pos[1].between?(0,8)}
+        moves.select {|pos| pos[0].between?(0,7) && pos[1].between?(0,7)} #array of possible moves given 8x8 board
     end
 
     def new_move_positions(pos)
-        @considered_positions += KnightPathFinder.valid_moves(pos).reject {|e| @considered_positions.include?(e)}
-        @considered_positions
+        new_pos_array = KnightPathFinder.valid_moves(pos).reject {|e| @considered_positions.include?(e)} #[1,2], [2,1]
+        # @considered_positions += new_pos_array
+        new_pos_array
+        
+        # Option 1
+        # new_pos.each do |child_pos| 
+        #     poss_moves = KnightPathFinder.valid_moves(child_pos)
+        #     poss_moves.each do |child_pos2|
+        #       build_move_tree
     end
 
-    def initialize(starting_position)
+    def initialize(starting_position) #[0,0]
 
         @root_node = PolyTreeNode.new(starting_position)
-        @considered_positions = [starting_position] + KnightPathFinder.valid_moves(starting_position)
-        build_move_tree(@root_node)
+        @considered_positions = [@root_node.value]
+        build_move_tree(@root_node) #[0,0]
 
         
     end
 
-    def build_move_tree(node)
-       KnightPathFinder.valid_moves(node.value).each do |pos|
-        node.add_child(PolyTreeNode.new(pos))
-       end
-
-    #    node.children.each do |child_node|
-    #     new_move_positions(child_node.value)
-    #     child_node.add_child(PolyTreeNode.new(child_node.value))
-    #    end
+    def build_move_tree(node) #[0,0]
+        # start with queue of JUST the root node, check what are the new_move_pos available? add each of those as a child.
+        # shift an element out, check it, if not, add its children to new_move_post
+        if node.nil?
+            return []
+        else
+            @considered_positions << new_move_positions(node.value)
+            @considered_positions.each do |child_pos|
+                child = node.add_child(PolyTreeNode.new(child_pos))
+                # @considered_positions << child_pos
+            end
+            node.children.each {|c| build_move_tree(c)}
+        end
 
     end
 
@@ -52,4 +66,4 @@ end
 
 k = KnightPathFinder.new([0,0])
 # p k.new_move_positions([2,1])
-p k
+p k.considered_positions
